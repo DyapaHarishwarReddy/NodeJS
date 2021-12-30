f640c257a0b4e2a0ba56b2dab245104b837e1269
DyapaHarishwarReddy <harishwar72@gmail.com>
2021-12-30 12:56:26 +0530

    This is my assignment2

---
+++ assignments/assignment_2/index.js
@@ -0,0 +1,13 @@
+const http = require("http");
+const fs = require("fs");
+fs.writeFile("index.html", "<h1>Hello World</h1>", (err) => {
+    console.log(err);
+});
+http.createServer((req, res) => {
+    var data = fs.readFileSync("index.html", 'utf-8');
+    console.log(data)
+    res.writeHead(200, { 'Content-type': 'text/html' });
+    res.end(data);
+}).listen(3000, () => {
+    console.log("server is listening to 3000");
+});
